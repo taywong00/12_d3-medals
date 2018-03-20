1a03f60659397b7f1fb3349382353b2643b41231
Taylor Wong <Taylor Wong>
2018-03-20 01:33:41 -0400

    ok done

--- medals.js
+++ medals.js
@@ -21,14 +21,14 @@ var usa = [9, 8, 6]; //usa: 9 gold, 8 silver, 6 bronze
 //create dot with all attributes
 //note: medalNum * 10 = radius of circle
 var drawDot = function(cx, cy, medalNum, color) {
-  var dot = pic.append('circle').attr('cx', cx).attr('cy', cy).attr('r', medalNum * 5).attr('fill', color);
+  var dot = pic.append('circle').attr('cx', cx).attr('cy', cy).attr('r', medalNum * 6).attr('fill', color);
 
 }
 
 //display the dots (trying to stay DRY: Dont Repeat Yourself)
 var display = function(country) {
   drawDot(width * 1/2, height * 1/4, country[0], '#FFD700'); //gold
-  drawDot(width * 1/4, height * 1/2, country[1], '#C0C0C0'); //silver
+  drawDot(width * 1/4, height * 3/4, country[1], '#C0C0C0'); //silver
   drawDot(width * 3/4, height * 3/4, country[2], '#CD7F32'); //bronze
 }
 
